44993eca74bac5752d10dbe35d8ecadd610516a9
Alex Delgado <gadc1996@gmail.com>
2021-01-05 11:35:44 -0700

    Creada funcion renderCard en index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php 
-    function writeMsg(){
-        echo 'Hello world!';
-    }
+    function renderCard($cardSize){
+?>
+        <div class="card-<?php echo $cardSize; ?>">
+                    <div class="category"><?php the_category(); ?></div>
+                    <a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="" /></a>
+                    <div class="card-info"><?php the_author(); ?></div>
+                    <a href="<?php the_permalink(); ?>"><h3 class="card-title"><?php the_title(); ?></h3></a>
+                    <div class="card-description"><?php the_excerpt(); ?></div>
+        </div>
+<?php
+        }
 ?>
 <?php get_header();?>
 <!--Inicia imagen destacada-->
@@ -32,17 +40,7 @@
 
         <?php 
             if($i <= 1){
-        ?>
-                <div class="card-big">
-                    <div class="category"><?php the_category(); ?></div>
-                    <a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="" /></a>
-                    <div class="card-info"><?php the_author(); ?></div>
-                    <a href="<?php the_permalink(); ?>"><h3 class="card-title"><?php the_title(); ?></h3></a>
-                    <div class="card-description"><?php the_excerpt(); ?></div>
-
-                </div>
-        <?php 
-                writeMsg();
+                renderCard('big');
                 $i++;
             }    
             }
